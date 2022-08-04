e91ddcced651068854b62d2ed4cbf86d50e438ef
dmlacerte <dmlacerte@gmail.com>
2022-08-03 22:37:12 -0400

    skills formatting

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,92 +2,66 @@ import React from 'react';
 import '../App.css';
 import styles from '../css/Skills.module.css';
 
-function Skills({techStack, selectedTechStack, setSelectedTechStack}) {
+function Skills({ techStack, selectedTechStack, setSelectedTechStack }) {
 
-    const updateTechStack= (tech) => {
+    const updateTechStack = (tech) => {
         let newStack = [...selectedTechStack];
         newStack.push(tech);
-        console.log(newStack);
 
         setSelectedTechStack(newStack);
     }
-    
+
+    const techSpace = (tech) => {
+        return (
+            <div
+                onClick={() => updateTechStack(tech)}
+                className={styles.techRowSpace}
+            >
+
+                <img
+                    className={selectedTechStack.includes(tech) ? styles.iconImgsSelected : styles.iconImgs}
+                    src={'/techIcons/' + tech.toLowerCase().replace(/\s/g, '') + '.png'}
+                    alt={tech + ' logo'} />
+
+                <p>{tech}</p>
+            </div>
+        )
+    }
+
     return (
         <section id="skills" className="pageSection">
             <h1 className='sectionHeader'>
                 Skills
             </h1>
-            <p className='sectionSubHeader'>
-                Select any of the technologies below to filter onto projects containing one or more of those technologies.
+            <div>
+                <p className={styles.categoryTitle}>Languages</p>
+                <div className={styles.techRow}>
+                    {techStack.languages.map(tech => techSpace(tech))}
+                </div>
+            </div>
+            <div>
+                <p className={styles.categoryTitle}>Libraries and Frameworks</p>
+                <div className={styles.techRow}>
+                    {techStack.libraries.map(tech => techSpace(tech))}
+                </div>
+            </div>
+            <div className={styles.multiCategoryRow}>
+                <div>
+                    <p className={styles.categoryTitle}>Databases</p>
+                    <div className={styles.techRow}>
+                        {techStack.databases.map(tech => techSpace(tech))}
+                    </div>
+                </div>
+                <div>
+                    <p className={styles.categoryTitle}>Other</p>
+                    <div className={styles.techRow}>
+                        {techStack.other.map(tech => techSpace(tech))}
+                    </div>
+                </div>
+            </div>
+            <p className={styles.subHeader + ' sectionSubHeader'}>
+                Want to see more of any of the above? Click on each to filter the apps section!
             </p>
-            <div className={styles.categoryRow}>
-                <p>Languages</p>
-                <div className={styles.techRow}>
-                    {techStack.languages.map(tech => (
-                        <div
-                            onClick={() => updateTechStack(tech)}
-                            className={styles.techRowSpace}
-                        >
-                            <img 
-                                className={styles.iconImgs}
-                                src={'/techIcons/' + tech.toLowerCase().replace(/\s/g, '') + '.png'} 
-                                alt={tech + ' logo'} />
-                            <p>{tech}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className={styles.categoryRow}>
-                <p>Libraries and Frameworks</p>
-                <div className={styles.techRow}>
-                    {techStack.libraries.map(tech => (
-                        <div
-                            onClick={() => updateTechStack(tech)}
-                            className={styles.techRowSpace}
-                        >
-                            <img 
-                                className={styles.iconImgs}
-                                src={'/techIcons/' + tech.toLowerCase().replace(/\s/g, '') + '.png'} 
-                                alt={tech + ' logo'} />
-                            <p>{tech}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className={styles.categoryRow}>
-                <p>Databases</p>
-                <div className={styles.techRow}>
-                    {techStack.databases.map(tech => (
-                        <div
-                            onClick={() => updateTechStack(tech)}
-                            className={styles.techRowSpace}
-                        >
-                            <img 
-                                className={styles.iconImgs}
-                                src={'/techIcons/' + tech.toLowerCase().replace(/\s/g, '') + '.png'} 
-                                alt={tech + ' logo'} />
-                            <p>{tech}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className={styles.categoryRow}>
-                <p>Other</p>
-                <div className={styles.techRow}>
-                    {techStack.other.map(tech => (
-                        <div
-                            onClick={() => updateTechStack(tech)}
-                            className={styles.techRowSpace}
-                        >
-                            <img 
-                                className={styles.iconImgs}
-                                src={'/techIcons/' + tech.toLowerCase().replace(/\s/g, '') + '.png'} 
-                                alt={tech + ' logo'} />
-                            <p>{tech}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
         </section>
     )
 }
