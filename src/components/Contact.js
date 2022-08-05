7e393b5e82e69bd57ef1c80af76d93ce0c51d769
dmlacerte <dmlacerte@gmail.com>
2022-08-05 17:08:05 -0400

    added contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import styles from '../css/Contact.module.css';
 
 function Contact() {
+    const hovered = {};
+
+    const [isHovered, setIsHovered] = useState(hovered);
+
+    const toggleOn = (name) => {
+        const newHover = {};
+        newHover[name] = true;
+        setIsHovered(newHover);
+    }
+
+    const toggleOff = () => {
+        setIsHovered({});
+    }
+    
     return (
-        <section id="contact" className="pageSection">
+        <section id="contact" className={styles.contactContainer + ' pageSection'}>
             <h1 className='sectionHeader'>
                 Contact Me
             </h1>
-            <p className='sectionSubHeader'>
+            <p className={styles.subHeader + ' sectionSubHeader'}>
                 Have any questions or want to work together? Let's get in touch!
             </p>
+            <div className={styles.contactLinksContainer}>
+                        <a
+                            href='https://www.linkedin.com/in/deanna-lacerte/'
+                            target="_blank"
+                            onMouseEnter={() => toggleOn('linkedin')}
+                            onMouseLeave={() => toggleOff()}
+                        >
+                            <div className={styles.linkContainer}>
+                                {isHovered['linkedin']
+                                    ? <img src='/socialIcons/linkedinBlack.png' alt='linkedin logo' className={styles.socialIcon} />
+                                    : <img src='/socialIcons/linkedinWhite.png' alt='linkedin logo' className={styles.socialIcon} />
+                                }
+                                <p>LinkedIn</p>
+                            </div>
+                        </a>
+                        <a
+                            href='mailto:dmlacerte@gmail.com'
+                            target="_blank" className={styles.rightLink}
+                            onMouseEnter={() => toggleOn('email')}
+                            onMouseLeave={() => toggleOff()}
+                        >
+                            <div className={styles.linkContainer}>
+                                {isHovered['email']
+                                    ? <img src='/socialIcons/mailBlack.png' alt='email logo' className={styles.socialIcon} />
+                                    : <img src='/socialIcons/mailWhite.png' alt='email logo' className={styles.socialIcon} />
+                                }
+                                <p>Email</p>
+                            </div>
+                        </a>
+                    </div>
         </section>
     )
 }
